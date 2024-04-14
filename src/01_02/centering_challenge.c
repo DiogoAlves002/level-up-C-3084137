664cb80ef7c6f01f4269f0ce72db6bd50ce93cf5
Diogo Alves <76565133+DiogoAlves002@users.noreply.github.com>
2024-04-14 17:00:22 +0000

    challenge 02 done

--- src/01_02/centering_challenge.c
+++ src/01_02/centering_challenge.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 void center_text(int width, char *text)
 {
-	/*
-	   - write this function to complete the challenge
-	   - this code won't run until the function is
-	   written
-	*/
+	int text_len = strlen(text);
+
+	if (text_len >= width - 2) // check if there's enough room for padding
+	{
+		printf("%s\n", text);
+	}
+	else
+	{
+		int padd = (width - text_len) / 2;
+		printf("%*s", padd, ""); // pads an enpty string
+		printf("%s\n", text);		 // puts the text after
+	}
 }
 
 int main()
 {
 	/* some sample text to center */
 	char *title[] = {
-	   "March Sales",
-	   "My First Project",
-	   "Centering output is so much fun!",
-	   "This title is very long, just to see whether the code can handle such a long title"
-	};
+			"March Sales",
+			"My First Project",
+			"Centering output is so much fun!",
+			"This title is very long, just to see whether the code can handle such a long title"};
 	int x;
 
 	/* output each of the strings, centered */
-	for( x=0; x<4; x++ )
+	for (x = 0; x < 4; x++)
 	{
 		/* assume the screen width to be 80 characters */
-		center_text( 80, title[x] );
+		center_text(80, title[x]);
 	}
 
-	return(0);
+	return (0);
 }
