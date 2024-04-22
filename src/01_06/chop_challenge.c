e536cd528b4b6b326719de07601a752ecea32e1e
DiogoAlves002 <diogoasilva@ua.pt>
2024-04-22 23:38:24 +0100

    challenge 6 done, screw freeing memory ig

--- src/01_06/chop_challenge.c
+++ src/01_06/chop_challenge.c
@@ -1,16 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int strLen(char *s){
+	int s_len = 0;
+	char s_c = 'a';
+
+	while (s_c != '\0'){
+		s_c = s[s_len++];
+	}
+	s_len--; // not counting the null terminator
+
+	return s_len;
+}
+
+
 char *left(char *s,int len)
 {
+	char *ret;
+
+	ret = (char*)malloc(sizeof(s[0] * len + 1));
+
+	for (int i = 0; i < len; i++){
+		ret[i] = s[i];
+	}
+
+	ret[len] = '\0';
+	return ret;
 }
 
 char *right(char *s,int len)
 {
+	char *ret;
+
+	ret = (char*)malloc(sizeof(s[0] * len + 1));
+
+
+	// get string length
+	int s_len = strLen(s);
+
+
+	int idx = 0;
+	for (int i = s_len - len; i < s_len; i++){
+		ret[idx++] = s[i];
+	}
+
+	ret[len] = '\0';
+	return ret;
 }
 
+/* 
+	assuming offset + len < strLen(s)
+  	(for simplicity)
+*/
 char *mid(char *s,int offset, int len)
 {
+	
+	char *ret;
+
+	ret = (char*)malloc(sizeof(s[0] * len + 1));
+
+	int idx = 0;
+	for (int i = offset - 1; i < offset + len; i++){
+		ret[idx++] = s[i];
+	}
+
+	ret[len] = '\0';
+	return ret;
+
 }
 
 int main()
@@ -21,6 +77,7 @@ int main()
 	printf("Left %d characters: %s\n",16,left(string,16));
 	printf("Right %d characters: %s\n",18,right(string,18));
 	printf("Middle %d characters: %s\n",11,mid(string,13,11));
+
 
 	return(0);
 }
