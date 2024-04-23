2349ecf9267cd9144ebdfe16a06f546bbff02b42
DiogoAlves002 <diogoasilva@ua.pt>
2024-04-23 10:15:49 +0100

    challenge 8 done

--- src/01_08/shuffle_challenge.c
+++ src/01_08/shuffle_challenge.c
@@ -3,6 +3,27 @@
 #include <time.h>
 
 #define PSIZE 20
+#define RSIZE 15
+
+
+int isInArray(int r, int* arr, int size){
+	for (int i = 0; i < size; i++){
+		if (arr[i] == r){
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+void removeFirstInArray(int *arr, int size){
+	for (int i = 1; i < size; i++){
+		arr[i - 1] = arr[i];
+	}
+
+	arr[size - 1] = -1;
+}
+
 
 int main()
 {
@@ -21,17 +42,31 @@ int main()
 
 	/* initialize */
 	srand( (unsigned)time(NULL) );
-	for(x=0;x<PSIZE;x++)
+	for(x=0;x<PSIZE;x++){
 		frequency[x] = 0;
+	}
+
+	int recent[RSIZE];
+	for (int i = 0; i < RSIZE; i++){
+		recent[i] = -1;
+	}
 
 
 	/* run through the playlist randomly */
 	puts("Playlist:");
 	count = 0;
+	r = -1;
 	while(count<100)
 	{
-		/* does r appear in recent[]? */
-		r = rand() % PSIZE;		/* random value */
+
+		while (isInArray(r, recent, RSIZE)){
+			r = rand() % PSIZE;		/* random value */
+		} 
+
+		removeFirstInArray(recent, RSIZE);
+		recent[RSIZE - 1] = r;
+
+		
 		printf("%3d: Now Playing '%s'\n",
 				count+1,
 				playlist[r]
